3cadeeff2b7fec78e92b24fbf1e8ff83468e94df
Subhed Chavan <subhedchavan@Subheds-MacBook-Pro-2.local>
2025-03-02 13:13:25 -0800

    Adding code for Dp-5

--- Problem_1.cpp
+++ Problem_1.cpp
@@ -1,6 +1,37 @@
 // Author: Subhed Chavan
 // Batch: December 24
-// Problem Statement: 
-// Approach: 
-// Time Complexity: 
-// Space Complexity: 
+// Problem Statement: Word Break
+// Approach: Dynamic Programming with HashSet
+// Time Complexity: O(N^2)
+// Space Complexity: O(N)
+
+class Solution {
+    private:
+        unordered_set<string> set;
+    
+    public:
+        bool wordBreak(string s, vector<string>& wordDict) {
+            if (s.empty() || wordDict.empty()) {
+                return false;
+            }
+    
+            set = unordered_set<string>(wordDict.begin(), wordDict.end());
+            int n = s.length();
+            vector<bool> dp(n + 1, false);
+            dp[0] = true;  
+    
+            for (int i = 1; i <= n; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (dp[j]) {  
+                        if (set.find(s.substr(j, i - j)) != set.end()) {  
+                            dp[i] = true;
+                            break;
+                        }
+                    }
+                }
+            }
+    
+            return dp[n];  
+        }
+    };
+    
